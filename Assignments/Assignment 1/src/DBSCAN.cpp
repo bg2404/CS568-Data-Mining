2582a3ef5ada3a7b2f1e680a48198f84e058987a
Bhasker Goel <bhasker2000goel@gmail.com>
2021-02-02 20:12:57 +0530

    Added DBSCAN

---
+++ Assignments/Assignment 1/src/DBSCAN.cpp
@@ -0,0 +1,97 @@
+#include "DBSCAN.h"
+
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+DBSCAN::DBSCAN(vector<Point> points, uint subspace, double eps, uint minPts) : m_points(points), m_subspace(subspace), m_eps(eps), m_minPts(minPts) {
+    m_numPoints = points.size();
+    m_clusterIDs.clear();
+    m_clusters.clear();
+}
+DBSCAN::~DBSCAN() {}
+
+uint DBSCAN::getSubspace() { return m_subspace; }
+
+double DBSCAN::getEps() { return m_eps; }
+
+uint DBSCAN::getMinPts() { return m_minPts; }
+
+vector<Point> DBSCAN::getPoints() { return m_points; }
+
+uint DBSCAN::getNumPoints() { return m_numPoints; }
+
+vector<vector<Point> > DBSCAN::getClusters() {
+    if (m_clusters.empty()) {
+        int clusterID = 1;
+        m_clusterIDs.resize(m_numPoints, UNCLASSIFIED);
+        for (uint i = 0; i < m_numPoints; ++i) {
+            if (m_clusterIDs[i] == UNCLASSIFIED) {
+                if (expandCuster(i, clusterID) == SUCCESS) {
+                    ++clusterID;
+                }
+            }
+        }
+        m_clusters.resize(clusterID - 1);
+        for (int i = 0; i < m_numPoints; ++i) {
+            m_clusters[m_clusterIDs[i] - 1].push_back(m_points[i]);
+        }
+    }
+    return m_clusters;
+}
+
+int DBSCAN::expandCuster(int idx, uint clusterID) {
+    vector<int> seeds = rangeQuery(m_points[idx]);
+    if (seeds.size() < m_minPts) {
+        m_clusterIDs[idx] = NOISE;
+        return FAILURE;
+    } else {
+        queue<int> qseeds;
+        for (uint i = 0; i < seeds.size(); ++i) {
+            m_clusterIDs[seeds[i]] = clusterID;
+            if (seeds[i] != idx) {
+                qseeds.push(seeds[i]);
+            }
+        }
+
+        while (!qseeds.empty()) {
+            int currentP = qseeds.front();
+            vector<int> result = rangeQuery(m_points[currentP]);
+            if (result.size() >= m_minPts) {
+                for (int resultP : result) {
+                    if (m_clusterIDs[resultP] == UNCLASSIFIED ||
+                        m_clusterIDs[resultP] == NOISE) {
+                        if (m_clusterIDs[resultP] == UNCLASSIFIED) {
+                            qseeds.push(resultP);
+                        }
+                        m_clusterIDs[resultP] = clusterID;
+                    }
+                }
+            }
+            qseeds.pop();
+        }
+        return SUCCESS;
+    }
+}
+
+vector<int> DBSCAN::rangeQuery(Point p) {
+    vector<int> result;
+    for (int i = 0; i < m_numPoints; ++i) {
+        if (dist(p, m_points[i]) <= m_eps) {
+            result.push_back(i);
+        }
+    }
+    return result;
+}
+
+double DBSCAN::dist(Point p1, Point p2) {
+    uint64_t d = 0;
+    uint dim = 1;
+    for (uint i = 0; i < 32; ++i, dim <<= 1) {
+        if (dim & m_subspace) {
+            d += ((long long)p1[i] - p2[i]) * (p1[i] - p2[i]);
+        }
+    }
+    return sqrt(d);
+}
---
+++ Assignments/Assignment 1/src/DBSCAN.h
@@ -0,0 +1,42 @@
+#ifndef DBSCAN_H
+#define DBSCAN_H
+
+#include <cstdlib>
+#include <vector>
+
+#define SUCCESS 0
+#define UNCLASSIFIED -1
+#define NOISE -2
+#define FAILURE -3
+
+using namespace std;
+
+typedef vector<int> Point;
+
+using Value = std::pair<Point, uint>;
+
+class DBSCAN {
+    vector<Point> m_points;
+    double m_eps;
+    uint m_minPts;
+    vector<int> m_clusterIDs;
+    vector<vector<Point> > m_clusters;
+    uint m_numPoints;
+    uint m_subspace;
+
+    int expandCuster(int, uint);
+    vector<int> rangeQuery(Point);
+    double dist(Point, Point);
+
+   public:
+    DBSCAN(vector<Point> points, uint m_subspace, double eps, uint minPts);
+    ~DBSCAN();
+    uint getSubspace();
+    double getEps();
+    uint getMinPts();
+    vector<Point> getPoints();
+    uint getNumPoints();
+    vector<vector<Point> > getClusters();
+};
+
+#endif
