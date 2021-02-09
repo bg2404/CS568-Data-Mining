043b559ebe5fbcc523a5c9c5b86136446ba1c61b
Anirudh-1149 <anirudh18@iitg.ac.in>
2021-02-09 19:38:46 +0530

    Changes to silhouette

--- Assignments/Assignment 1/src/silhouette.py
+++ Assignments/Assignment 1/src/silhouette.py
@@ -1,10 +1,16 @@
 from sklearn.metrics import silhouette_score
 from numpy import genfromtxt
+import numpy as np
 import glob
 
 for filename in glob.iglob('Subspace*.csv'):
     data = genfromtxt(filename, delimiter=' ')
     class_label = data[:, -1]
+    class_label = class_label.astype(int)
     dataset = data[:, :-1]
-    silhouette_avg = silhouette_score(dataset, class_label)
-    print(filename, silhouette_avg)
+    y = np.bincount(class_label)
+    if y.size > 1:
+        silhouette_avg = silhouette_score(dataset, class_label)
+        print(filename, silhouette_avg)
+    else:
+        print("1")
